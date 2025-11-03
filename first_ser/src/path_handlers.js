171012a3293ee612570609614924a1e86523dde0
Natalia Gruszczynska <natip29b@gmail.com>
2025-11-03 08:36:50 +0000

    cs

---
+++ first_ser/src/path_handlers.js
@@ -0,0 +1,46 @@
+import { readFile, readFileSync } from "node:fs";
+
+const index_html = readFileSync("static/index.html");
+const favicon = readFileSync("favicon.ico");
+const pathConfigs = [
+  {
+    path: "/",
+    allowed_methods: ["GET"],
+    handler: (req, res) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(index_html);
+    },
+  },
+  {
+    path: "/hello",
+    allowed_methods: ["GET"],
+    handler: (req, res) => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("hello world!\n");
+    },
+
+  },
+  {
+     path: "/favicon.ico",
+    allowed_methods: ["GET"],
+    handler: (req, res) => {
+      res.writeHead(200, { "Content-Type": "image/vnd.microsoft.icon" });
+      res.end(favicon);
+    },
+
+  }
+];
+
+export function handlePath(path, req, res) {
+  for (let config of pathConfigs) {
+    if (path === config.path) {
+      if (config.allowed_methods.includes(req.method)) {
+        config.handler(req, res);
+      } else {
+        res.writeHead(405, { "Content-Type": "text/plain" });
+        res.end("Method not allowed\n");
+      }
+      break;
+    }
+  }
+}
--- projekt2/index.js
+++ projekt2/index.js
@@ -1,16 +1,78 @@
+// import express from "express";
+// import ejs from "ejs";
+
+// const port = 8000;
+// const card_categories = ["j. angielski - food", "stolice europejskie"];
+
+// const app = express();
+
+// app.set("view engine", "ejs");
+// console.log(ejs.name);
+
+// app.use(express.static("public"));
+
+// app.get("/cards/categories/", (req, res) => {
+//   res.render("categories", {
+//     title: "Kategorie",
+//     categories: card_categories,
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening on http://localhost:${port}`);
+// });
+
 import express from "express";
 
 const port = 8000;
+const card_categories = {
+  "j-angielski-food": {
+    name: "j. angielski - food",
+    cards: [
+      { front: "truskawka", back: "strawberry" },
+      { front: "gałka muszkatołowa", back: "nutmeg" },
+      { front: "jabłko", back: "apple" },
+      { front: "karczoch", back: "artichoke" },
+      { front: "cielęcina", back: "veal" },
+    ],
+  },
+  "stolice-europejskie": {
+    name: "stolice europejskie",
+    cards: [
+      { front: "Holandia", back: "Amsterdam" },
+      { front: "Włochy", back: "Rzym" },
+      { front: "Niemcy", back: "Berlin" },
+      { front: "Węgry", back: "Budapeszt" },
+      { front: "Rumunia", back: "Bukareszt" },
+    ],
+  },
+};
 
-const app = express(); 
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => { 
-  res.set("Content-Type", "text/plain"); 
-  res.send("Hello world"); 
+app.get("/cards/categories/", (req, res) => {
+  res.render("categories", {
+    title: "Kategorie",
+    categories: Object.entries(card_categories).map(
+      ([id, category]) => category.name
+    ),
+  });
 });
 
-app.listen(port, () => { // Startujemy serwer na porcie 8000
+app.get("/cards/:category_id", (req, res) => {
+  if (card_categories.hasOwnProperty(req.params.category_id)) {
+    const category = card_categories[req.params.category_id];
+    res.render("category", {
+      title: category.name,
+      category,
+    });
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
-
-//changes?
