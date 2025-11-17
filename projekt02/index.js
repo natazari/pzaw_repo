b82fab727556ec82d04bcb0047cffba06eeafc6e
natazari <natip29b@gmail.com>
2025-11-17 02:43:45 +0000

    dom2

--- projekt02/index.js
+++ projekt02/index.js
@@ -1,75 +1,61 @@
-// import express from "express";
-// import ejs from "ejs";
-
-// const port = 8000;
-// const card_categories = ["j. angielski - food", "stolice europejskie"];
-
-// const app = express();
-
-// app.set("view engine", "ejs");
-// console.log(ejs.name);
-
-// app.use(express.static("public"));
-
-// app.get("/cards/categories/", (req, res) => {
-//   res.render("categories", {
-//     title: "Kategorie",
-//     categories: card_categories,
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server listening on http://localhost:${port}`);
-// });
-
 import express from "express";
-
+import flashcards from "./models/flashcards.js";
 const port = 8000;
-const card_categories = {
-  "j-angielski-food": {
-    name: "j. angielski - food",
-    cards: [
-      { front: "truskawka", back: "strawberry" },
-      { front: "gałka muszkatołowa", back: "nutmeg" },
-      { front: "jabłko", back: "apple" },
-      { front: "karczoch", back: "artichoke" },
-      { front: "cielęcina", back: "veal" },
-    ],
-  },
-  "stolice-europejskie": {
-    name: "stolice europejskie",
-    cards: [
-      { front: "Holandia", back: "Amsterdam" },
-      { front: "Włochy", back: "Rzym" },
-      { front: "Niemcy", back: "Berlin" },
-      { front: "Węgry", back: "Budapeszt" },
-      { front: "Rumunia", back: "Bukareszt" },
-    ],
-  },
-};
+
+
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded());
 
 app.get("/cards/categories/", (req, res) => {
   res.render("categories", {
     title: "Kategorie",
-    categories: Object.entries(card_categories).map(
-      ([id, category]) => category.name
-    ),
+    categories: flashcards.getCategorySummaries(),
   });
 });
 
 app.get("/cards/:category_id", (req, res) => {
-  if (card_categories.hasOwnProperty(req.params.category_id)) {
-    const category = card_categories[req.params.category_id];
+  var category_id = req.params.category_id;
+  var category = flashcards.getCategory(category_id);
+  if (category != null) {
+   
     res.render("category", {
       title: category.name,
       category,
+      
     });
   } else {
     res.sendStatus(404);
+  }
+});
+
+app.post("/cards/:category_id/new", (req, res) => {
+  const category_id = req.params.category_id;
+  if (!flashcards.hasCategory(category_id)) {
+    res.sendStatus(404);
+  } else {
+    let card_data = {
+      front: req.body.front,
+      back: req.body.back,
+    };
+    var errors = flashcards.validateCardData(card_data);
+    if (errors.length == 0) {
+      flashcards.addCard(category_id, card_data);
+      res.redirect(`/cards/${category_id}`);
+    } else {
+      res.status(400);
+      res.render("new_card", {
+        errors,
+        title: "Nowa fiszka",
+        front: req.body.front,
+        back: req.body.back,
+        category: {
+          id: category_id,
+        },
+      });
+    }
   }
 });
 
